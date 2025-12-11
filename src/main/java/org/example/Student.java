ec44e98f89e8f0211096e5d4e79144e00d78c8a2
Recep Borekci <recep.borekci@qaracter.com>
2025-12-11 12:23:31 +0300

    Add comment to the Student class

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -3,9 +3,11 @@ package org.example;
 public class Student {
     String name;
     int grade;
+    String comment;
 
-    public Student(String name, int grade) {
+    public Student(String name, int grade,  String comment) {
         this.name = name;
         this.grade = grade;
+        this.comment = comment;
     }
 }
