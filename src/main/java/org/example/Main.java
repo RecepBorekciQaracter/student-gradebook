72836dca80e8e87505f157da8d3c775a2c77b0f6
Recep Borekci <recep.borekci@qaracter.com>
2025-12-11 11:58:45 +0300

    Add main menu structure with input validation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,73 @@
 package org.example;
 
 
-public class Main {
-    static void main() {
+import java.util.Scanner;
 
+public class Main {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Welcome to the Student Gradebook app! Please select an option: ");
+
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Delete Student");
+            System.out.println("4. Get Highest Note");
+            System.out.println("5. Get Lowest Note");
+            System.out.println("6. Get Class Average");
+            System.out.println("7. Get Report");
+            System.out.println("8. Exit");
+
+            int choice = readInt(sc, "\nPlease enter your choice: ");
+
+            switch (choice) {
+                case 1:
+                    // Add Student
+                    // addStudent();
+                    break;
+                case 2:
+                    // View Students
+                    // viewStudents();
+                    break;
+                case 3:
+                    // Delete Student
+                    // deleteStudent();
+                    break;
+                case 4:
+                    // Get Highest Note
+                    // getHighestNote();
+                    break;
+                case 5:
+                    // Get Lowest Note
+                    // getLowestNote();
+                    break;
+                case 6:
+                    // Get Report (Print highest note, lowest note, and average of the class using string builder)
+                    // getReport();
+                    break;
+                case 8:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid option.\n");
+
+            }
+        }
+
+    }
+
+    public static int readInt(Scanner sc, String message) {
+        while (true) {
+            System.out.print(message);
+            if (sc.hasNextInt()) {
+                int value = sc.nextInt();
+                sc.nextLine(); // clear leftover newline
+                return value;
+            } else {
+                System.out.println("Please enter a valid number.");
+                sc.nextLine(); // discard bad input
+            }
+        }
     }
 }
