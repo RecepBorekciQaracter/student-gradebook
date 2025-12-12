240fe950f48e84b2fafdb169c222891b9cb92451
Carlos Rivera <carlos.rivera@qaracter.com>
2025-12-12 09:58:17 +0000

    Student class modified, new methods also implemented

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,20 +1,63 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Student {
     String name;
-    double grade;
+    ArrayList<Double> gradesAL = new ArrayList<>();
     String comment;
 
-    public Student(String name, double grade,  String comment) {
+    public Student(String name, ArrayList<Double> grades,  String comment) {
         this.name = name;
-        this.grade = grade;
+        this.gradesAL = grades;
         this.comment = comment;
+    }
+
+    // Returns Highest grade of a student, -1.0 means no grades were stored
+    public Double highestGrade() {
+        Double highestGrade = -1.0;
+
+        for(int i = 0; i< gradesAL.size(); i++) {
+            if (gradesAL.get(i) > highestGrade) {
+                highestGrade = gradesAL.get(i);
+            }
+        }
+
+        return highestGrade;
+    }
+
+    // Returns Lowest grade of a Student, 1000000000.0 means no grades
+    public Double lowestGrade() {
+        Double lowestGrade = 1000000000000.0;
+
+        for(int i = 0; i< gradesAL.size(); i++) {
+            if (gradesAL.get(i) < lowestGrade) {
+                lowestGrade = gradesAL.get(i);
+            }
+        }
+
+        return lowestGrade;
+    }
+
+    // Returns Students grade average, -1.0 if no grades are stored
+    public Double average() {
+        Double average = -1.0;
+
+        if(gradesAL.size() > 0) {
+            average = 0.0;
+            for(int i = 0; i< gradesAL.size(); i++) {
+                average = average + gradesAL.get(i);
+            }
+            average = average/gradesAL.size();
+        }
+
+        return average;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Strudents name: ").append(name).append("\nStudent's grade: ").append(String.valueOf(grade)).append("\nComment: ").append(comment);
+        sb.append("Strudents name: ").append(name).append("\nStudent's grade: ").append(gradesAL.toString()).append("\nComment: ").append(comment);
         return sb.toString();
     }
 }
